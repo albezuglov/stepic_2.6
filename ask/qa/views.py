27d75a9702f5c35b2c42ef3f03fe681987f407bd
Alexandr Bezuglov <alexworldmail@gmail.com>
2016-03-11 22:55:53 +0200

    Initial

---
+++ ask/qa/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django.http import Http404
+from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .models import *
+
+# Create your views here.
+def test(request, *args, **kwargs):
+    return HttpResponse('OK')
+
+def defaultOk(request, *args, **kwargs):
+    return HttpResponse('OK')
+
+def posts_list(request, *args, **kwargs):
+    #questions = Post.objects.all().order_by("-added_at")
+    sort_by = kwargs.get('sort_by', '-id')
+    post_list = Question.objects.all().order_by("-rating" if sort_by == "rating" else "-id")
+    paginator = Paginator(post_list, 10)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request, 'posts_list.html', {'posts': posts})
+
+def question(request, *args, **kwargs):
+    try:
+        q_id = kwargs['id']
+        q = Question.objects.get(pk=q_id)
+    except (KeyError, Question.DoesNotExist ):
+        raise Http404("Question not found")
+
+    return render(request, 'question.html', {'question': q})    
+    
